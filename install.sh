18aa8c24597f4a8df9ba8538a5ebb6f198adbb6d
Frère Jacques <zyb@zjex.com>
2018-01-18 08:57:20 +0800

    go

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 wget https://dl.google.com/go/go1.9.2.windows-amd64.msi -P ./disted/
 wget https://dl.google.com/go/go1.9.2.src.tar.gz  -P ./disted/
-wget https://dl.google.com/go/go1.9.2.linux-amd64.tar.gz -P ./disted/
+#wget https://dl.google.com/go/go1.9.2.linux-amd64.tar.gz -P ./disted/
 #wget https://www.google.com/chrome/browser/thankyou.html?standalone=1&platform=win&installdataindex=defaultbrowser -P ./disted/
 ls -alh ./disted/
 ls -lh ./disted/*
