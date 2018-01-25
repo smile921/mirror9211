6ccadf480494f83f9cc08a9352cc47c04bb8d8e8
Frère Jacques <zyb@zjex.com>
2018-01-25 12:44:56 +0800

    ...

---
+++ scripts/convertBig2Small.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#set -e
+#set -x
+function main{
+    files=`ls`
+    for file in $files
+    do
+        #ls -lh $file
+        size=`du -sm $file | awk '{print $1}'`
+        #echo $size
+        if test -d $file
+        then
+            continue
+        fi
+
+        if [ $size -gt 28 ]
+        then
+            $(mkdir_and_split $file 28m )
+        fi
+    done
+}
+
+function mkdir_and_split{
+               size=$2
+               file=$1
+               if test -d __$file
+               then
+                   echo __$file 'existed';
+                   return 1
+               fi
+               mkdir __$file
+               mv $file __$file
+               cd __$file
+               split -b 28m $file $file
+               rm $file
+               cd ..
+               return 0
+}
+$(main)
+ls . -alh
