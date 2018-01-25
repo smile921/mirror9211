ad50d245049f21520cf9115d473c210fadc687a3
Frère Jacques <zyb@zjex.com>
2018-01-25 11:13:44 +0800

    ...

--- scripts/answer.sh
+++ scripts/answer.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/expect -f
 #HOME=$1
-HOME=/home/travis
-echo $HOME
+#HOME=/home/travis
+#echo $HOME
 set timeout -1
 pwd
-spawn  git clone git@gitee.com:mirror921/mirror921.git $HOME/release
+spawn  git clone git@gitee.com:mirror921/mirror921.git /home/travis/release
 expect "*yes"
 send "yes\n";
 interact
